d4170edf6aec7e8aba8b55a15094953e014d2751
markgordon22 <markgordon97@gmail.com>
2021-02-27 00:00:55 +0000

    h

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+var elements = document.getElementsByClassName("information")
+
+for(let i=0; i<elements.length; i++) {
+elements[i].style.display="none"}
+
+var buttons = document.getElementsByClassName("informationbutton")
+
+for (let q = 0; q<buttons.length; q++) {
+buttons[q].addEventListener("click", function(e){
+    e.preventDefault()
+    
+    var informationelement = buttons[q].previousElementSibling
+    if(informationelement.style.display === "none")
+    {
+    informationelement.style.display = "block"
+    }
+    else {
+    informationelement.style.display = "none"
+    }
+})
+}
+
+
+
+
+
