4d534b19f2a50d1b3fee8b6c50216d9bd239a952
markgordon22 <markgordon97@gmail.com>
2021-03-04 21:16:37 +0000

    llll

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,7 +24,7 @@ buttons[q].addEventListener("click", function(e){
 
 document.getElementById("hello-button").addEventListener("click", function(e){
   e.preventDefault()
-  document.getElementsByClassName("button-text")[0].innerHTML="welcome!"
+  document.getElementsByClassName("button-text")[0].innerHTML="Looking for a holiday somewhere in Ireland? Let us help you!"
 })
 
 
