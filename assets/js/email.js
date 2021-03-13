d83a71a5e3a9632f6ce5d41dcea451f4bcb92cfd
markgordon22 <markgordon97@gmail.com>
2021-03-13 17:52:41 +0000

    hghefgeu

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,5 +1,21 @@
 
-
+ function sendMail(contactForm) {
+       
+        emailjs.send("service_4b99rtc", "template_nllrsno",{
+        from_name: document.getElementById("fromname").value,
+        to_email: document.getElementById("toemail").value,
+        message: document.getElementById("msg").value,       
+        })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        }
+    );
+    return false;  // To block from loading a new page
+}
 
 
 
