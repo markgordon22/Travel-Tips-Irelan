94902eb56f8aca847f5a95a7228a54a1e6ba965d
markgordon22 <markgordon97@gmail.com>
2021-03-13 22:52:45 +0000

    klkl

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,22 +1,28 @@
 
 
-function sendMail(contactform) {
-       
-        emailjs.send("service_4b99rtc","template_nllrsno",{
-        "from_name": contactForm.firstname.value,
-        "lastname": contactForm.lastname.value,
-        "from_email": contactForm.email.value,
-        "phone": contactForm.phone.value,
-        "from": contactForm.from.value,
-        "to": contactForm.to.value,
-        "message": contactForm.message.value
-})
+function sendMail(params) {
+    var params = {
+        "from_name": contactform.firstname.value,
+        "lastname": contactform.lastname.value,
+        "from_email": contactform.email.value,
+        "phone": contactform.phone.value,
+        "from": contactform.from.value,
+        "to": contactform.to.value,
+        "message": contactform.message.value
+    };
+        emailjs.send("service_4b99rtc","template_nllrsno",this, params)
     .then(
+        // Alert sent if email successful
         function(response) {
-            console.log("SUCCESS", response);
+            alert("Your email has been sent, we will be in touch as soon as possible");
+            // Page reloads after user clicks "ok" on alert box
+            // Found on Stack Overflow, linked in README
+            window.location.reload(true);
         },
+        // Alert sent if email failed
         function(error) {
-            console.log("FAILED", error);
+            alert("Your email was not sent, please try again");
+            window.location.reload(true);
         }
     );
     return false;  // To block from loading a new page
